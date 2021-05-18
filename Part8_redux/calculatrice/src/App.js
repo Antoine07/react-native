fc78c8174de56445242a0a52e62a198628fafa2e
Antoine07 <antoine.lucsko@gmail.com>
2021-05-18 11:02:01 +0200

    react reduxé

--- Part8_redux/calculatrice/src/App.js
+++ Part8_redux/calculatrice/src/App.js
@@ -7,13 +7,17 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
 
+  // Hooks de Redux
   const { numbers } = useSelector( state => state ); // lire le store
   const dispatch = useDispatch(); // lancer une action dans le reducer Redux
 
   return (
     <div className="App">
-     <button>Afficher les nombres</button>
-     {/** affichez simple les nombres du store */}
+      {/** dans le dispatch lancer votre action qui modifiera l'ordre d'affichage 
+       * de vos numbers
+       */}
+     <button onClick={() => dispatch({}) } >Afficher les nombres</button>
+     {/** affichez simplement les nombres du store ul li */}
     </div>
   );
 }
